7b9db1a06da3aeae839af4330b81f2b177ce3cb8
giacomobranchi <branchigiacomo28@gmail.com>
2023-10-11 16:24:19 +0200

    add milestone 1

--- index.php
+++ index.php
@@ -3,3 +3,84 @@ Milestone 1
 Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. Scriviamo tutto (logica e layout) in un unico file index.php
 Milestone 2
 Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale -->
+
+<?php
+$passLength = $_GET['generator'];
+
+function genRandomPassword($length)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-.,-_:;`';
+    $charactersLength = strlen($characters);
+    $randomPass = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomPass .= $characters[random_int(0, $charactersLength - 1)];
+    }
+    return $randomPass;
+};
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand navbar-light bg-light py-2">
+        <div class="container">
+            <div class="nav navbar-nav">
+                <a class="nav-item nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+
+            </div>
+        </div>
+
+    </nav>
+    <div class="container py-4">
+        <h1 class="text-center">
+            Random Password Generator
+        </h1>
+        <div class="d-flex justify-content-around py-4">
+            <form action="" method="GET">
+                <input type="number" name="generator" id="generator">
+                <button type="submit">invia</button>
+            </form>
+            <div>
+                <h4>
+                    Your Password is: <span class=text-danger><?php echo genRandomPassword($passLength); ?></span>
+                </h4>
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
